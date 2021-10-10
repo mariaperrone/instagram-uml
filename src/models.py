addec0d67784f375856d70fa8d55bab269952c07
mariaperrone <perronemar148@gmail.com>
2021-10-10 15:04:17 +0000

    check relationships

--- src/models.py
+++ src/models.py
@@ -5,26 +5,51 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
+import enum 
+from sqlalchemy import Integer, Enum
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(20), nullable=False)
+    firstname = Column(String(20), nullable=False)
+    lastname = Column(String(20), nullable=False)
+    email = Column(String(50), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user = relationship("User")
+
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user = relationship("User")
+
+class TypeEnum(enum.Enum):
+    image=1
+    text=2
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type= Column(Enum(TypeEnum))
+    url= Column(String(50), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), primary_key=True)
+    post = relationship("Post")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(256), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user = relationship("User")
+    post_id = Column(Integer, ForeignKey('post.id'), primary_key=True)
+    post = relationship("Post")
 
     def to_dict(self):
         return {}
